40c350a623b3b0c909a9f2a980b7f7fcc43f4e84
Vectt0r <douglas_sd0@outlook.com>
2025-08-15 17:08:08 -0300

    primeira versão em prod

--- 261Lyrics-app/App.js
+++ 261Lyrics-app/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from './screens/HomeScreen';
 import LyricsScreen from './screens/LyricsScreen';
 import SetListsScreen from './screens/SetListsScreen';
 import MusicScreen from './screens/MusicScreen';
 import { MaterialIcons } from '@expo/vector-icons';
+import { enableScreens } from 'react-native-screens';
 import { FontSizeProvider } from './screens/FontSizeContext';
 import SetListMusicas from './screens/SetListMusicas'; // importe a nova tela
 import SetListLyricsScreen from './screens/SetListLyricsScreen'; // importe a nova tela
-
-import {Easing} from "react-native";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -66,48 +66,53 @@ function SetListsStack() {
     );
 }
 
+enableScreens();
+
 export default function App() {
     return (
-        <FontSizeProvider>
-            <NavigationContainer>
-                <Tab.Navigator
-                    screenOptions={{
-                        tabBarActiveTintColor: '#000000',
-                        tabBarInactiveTintColor: '#bab9b9',
-                    }}
-                >
-                    <Tab.Screen
-                        name="Home"
-                        component={HomeStack}
-                        options={{
-                            headerShown: false,
-                            tabBarIcon: ({ color, size }) => (
-                                <MaterialIcons name="home" color={color} size={size} />
-                            ),
+        <SafeAreaProvider>
+            <FontSizeProvider>
+                <NavigationContainer>
+                    <Tab.Navigator
+                        screenOptions={{
+                            tabBarActiveTintColor: '#000000',
+                            tabBarInactiveTintColor: '#bab9b9',
                         }}
-                    />
-                    <Tab.Screen
-                        name="SetLists"
-                        component={SetListsStack}
-                        options={{
-                            headerShown: false,
-                            tabBarIcon: ({ color, size }) => (
-                                <MaterialIcons name="playlist-play" color={color} size={size} />
-                            ),
-                        }}
-                    />
-                    <Tab.Screen
-                        name="Musicas"
-                        component={MusicasStack}
-                        options={{
-                            headerShown: false,
-                            tabBarIcon: ({ color, size }) => (
-                                <MaterialIcons name="music-note" color={color} size={size} />
-                            ),
-                        }}
-                    />
-                </Tab.Navigator>
-            </NavigationContainer>
-        </FontSizeProvider>
-    );
+                    >
+                        <Tab.Screen
+                            name="Home"
+                            component={HomeStack}
+                            options={{
+                                headerShown: false,
+                                tabBarIcon: ({ color, size }) => (
+                                    <MaterialIcons name="home" color={color} size={size} />
+                                ),
+                            }}
+                        />
+                        <Tab.Screen
+                            name="SetLists"
+                            component={SetListsStack}
+                            options={{
+                                headerShown: false,
+                                tabBarIcon: ({ color, size }) => (
+                                    <MaterialIcons name="playlist-play" color={color} size={size} />
+                                ),
+                            }}
+                        />
+                        <Tab.Screen
+                            name="Musicas"
+                            component={MusicasStack}
+                            options={{
+                                headerShown: false,
+                                tabBarIcon: ({ color, size }) => (
+                                    <MaterialIcons name="music-note" color={color} size={size} />
+                                ),
+                            }}
+                        />
+                    </Tab.Navigator>
+                </NavigationContainer>
+            </FontSizeProvider>
+        </SafeAreaProvider>
+
+            );
 }
